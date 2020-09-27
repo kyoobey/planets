76c58daa37cde23ef566cfc79fbbc087d1510dd3
tshrpl <hi.tusharpaul@gmail.com>
2020-09-27 20:50:15 +0530

    added biomes

--- Assets/ColorGenerator.cs
+++ Assets/ColorGenerator.cs
@@ -7,13 +7,14 @@ public class ColorGenerator {
 	ColorSettings settings;
 	Texture2D texture;
 	const int textureResolution = 50;
+	INoiseFilter boimeNoiseFilter;
 
 	public void UpdateSettings(ColorSettings settings) {
 		this.settings = settings;
-		if (texture == null)
-		{
-			texture = new Texture2D(textureResolution, 1);
+		if (texture == null || texture.height != settings.biomeColorSettings.biomes.Length) {
+			texture = new Texture2D(textureResolution, settings.biomeColorSettings.biomes.Length, TextureFormat.RGBA32, false);
 		}
+		boimeNoiseFilter = NoiseFilterFactory.CreateNoiseFilter(settings.biomeColorSettings.noise);
 	}
 
 	public void UpdateElevation(MinMax elevationMinMax) {
@@ -21,13 +22,42 @@ public class ColorGenerator {
 	}
 
 	public void UpdateColors() {
-		Color[] colors = new Color[textureResolution];
-		for (int i=0; i < textureResolution; i++) {
-			colors[i] = settings.gradient.Evaluate(i / (textureResolution - 1f));
+		Color[] colors = new Color[texture.width * texture.height];
+
+		int colorIndex = 0;
+		foreach (var biome in settings.biomeColorSettings.biomes) {
+			for (int i=0; i < textureResolution; i++) {
+				Color gradientColor = biome.gradient.Evaluate(i / (textureResolution - 1f));
+				Color tintColor = biome.tint;
+				colors[colorIndex] = gradientColor * (1 - biome.tintPercent) + tintColor * biome.tintPercent;
+				colorIndex++;
+			}
 		}
 		texture.SetPixels(colors);
 		texture.Apply();
 		settings.planetMaterial.SetTexture("_texture", texture);
+	}
+
+	public float BiomePercentFromPoint(Vector3 pointOnUnitSphere) {
+		float heightPercent = (pointOnUnitSphere.y + 1) / 2f;
+		heightPercent += (boimeNoiseFilter.Evaluate(pointOnUnitSphere) - settings.biomeColorSettings.noiseOffset) * settings.biomeColorSettings.noiseStrength;
+		float biomeIndex = 0;
+		int numBiomes = settings.biomeColorSettings.biomes.Length;
+		float blendRange = settings.biomeColorSettings.blendAmount + .001f;
+
+		for (int i=0; i<numBiomes; i++) {
+			// if (settings.biomeColorSettings.biomes[i].startHeight < heightPercent) {
+			// 	biomeIndex = i;
+			// } else {
+			// 	break;
+			// }
+			float distance = heightPercent - settings.biomeColorSettings.biomes[i].startHeight;
+			float weight = Mathf.InverseLerp(-blendRange, blendRange, distance);
+			biomeIndex *= (1-weight);
+			biomeIndex += i * weight;
+		}
+
+		return biomeIndex / Mathf.Max(1, numBiomes - 1);
 	}
 
 }
--- Assets/TerrainFace.cs
+++ Assets/TerrainFace.cs
@@ -26,7 +26,7 @@ public class TerrainFace {
 		Vector3[] vertices = new Vector3[resolution * resolution];
 		int[] triangles = new int[(resolution-1)*(resolution-1)*6];
 		int triIndex = 0;
-
+		Vector2[] _temp_uv = mesh.uv;
 
 		for (int y=0; y<resolution; y++) {
 			for (int x=0; x<resolution; x++) {
@@ -54,6 +54,26 @@ public class TerrainFace {
 		mesh.vertices = vertices;
 		mesh.triangles = triangles;
 		mesh.RecalculateNormals();
+		mesh.uv = _temp_uv;
+	}
+
+	public void UpdateUVs(ColorGenerator colorGenerator) {
+		Vector2[] uv = new Vector2[resolution*resolution];
+
+		for (int y=0; y<resolution; y++) {
+			for (int x=0; x<resolution; x++) {
+				int i = x + y*resolution;
+
+				Vector2 percent = new Vector2(x,y) / (resolution-1);
+				Vector3 pointOnUnitCube = localUp + (percent.x-.5f)*2*axisA + (percent.y-.5f)*2*axisB;
+				Vector3 pointOnUnitSphere = pointOnUnitCube.normalized;
+				// vertices[i] = shapeGenerator.CalculatePointOnPlanet(pointOnUnitSphere);
+
+				uv[i] = new Vector2(colorGenerator.BiomePercentFromPoint(pointOnUnitSphere), 0);
+			}
+		}
+
+		mesh.uv = uv;
 	}
 
 }
